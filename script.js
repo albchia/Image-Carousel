47bfe564a063feeda934b9a39bfe933c51f1860c
albchia <albertichiara@hotmail.com>
2020-11-13 22:28:22 +0100

    Update script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+const carouselSlide = document.querySelector('.carousel-slide');
+const carouselImages = document.querySelectorAll('.carousel-slide img');
+
+//Work on buttons
+const prevButt = document.querySelector('#prevButt');
+const nextButt = document.querySelector('#nextButt');
+
+//Add a counter to monitor on which image we're on
+let counter = 1;
