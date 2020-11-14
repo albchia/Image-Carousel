1d5545641292dc3e52a3ac72e2959887cdf88801
albchia <albertichiara@hotmail.com>
2020-11-14 15:15:05 +0100

    Update script.js

--- script.js
+++ script.js
@@ -14,4 +14,4 @@ const size = carouselImages[0].clientWidth;
 
 //We will start from the first image.
 //It wil move one picture at a time.
-carouselSlide.style.transform = 'translateX'( + (-size * counter) + 'px');
+carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
