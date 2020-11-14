04afccdd6d0d4d7bfe2f9f873dc63d12b66c57f2
albchia <albertichiara@hotmail.com>
2020-11-14 12:35:37 +0100

    Update script.js

--- script.js
+++ script.js
@@ -8,6 +8,10 @@ const nextButt = document.querySelector('#nextButt');
 //Add a counter to monitor on which image we're on
 let counter = 1;
 
-//Set the width to slide to know how do we need to slide
+//Set the width to slide to know how do we need to slide.
+//It will give us the original width back.
 const size = carouselImages[0].clientWidth;
 
+//We will start from the first image.
+//It wil move one picture at a time.
+carouselSlide.getElementsByClassName.transform = `translateX(${-size * counter}`
